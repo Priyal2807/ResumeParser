69be344cf54e95fa5a3b7a2fa14e5fed12491097
Priyal2807 <priyalsobti@gmail.com>
2022-07-07 19:30:54 +0530

    making changes as per the requirements

--- routes/parser.js
+++ routes/parser.js
@@ -1,20 +1,24 @@
-const multer = require('multer');
 const router = require('express').Router();
-const upload = multer({ dest: 'uploads/' })
 const fs = require('fs');
 const pdfParse = require('pdf-parse');
 
-router.post('/', upload.single('file'), (req, res) => {
-    getPdf(req.file.path);
-    res.send({message:"sucess"})
+router.post('/', (req, res) => {
+    try {
+        getPdf(req.files.file.data, req.body.skill);
+        res.send({ message: "sucess" });
+    } catch (error) {
+        res.status(500).send({message:"Internal Server Error"})
+    }
 })
 
-const getPdf = async (file) => {
-    let readFile = fs.readFileSync(file);
+const getPdf = async (file,skill) => {
+    
     try {
-        let pdfExtract = await pdfParse(readFile);
+        let pdfExtract = await pdfParse(file);
+        console.log(pdfExtract);
         let extract = pdfExtract.text;
-        let words = extract.split(/[,.\n\s+\t+]/);
+        let words = extract.split(/[,.\n\s+\t+]/)
+        console.log(words);
         let dict = {};
         words.forEach((word, i) => {
             dict[word] = i;
--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const { urlencoded } = require('express');
 const ourParser = require('./routes/parser.js');
+const fileUpload = require("express-fileupload");
 
 
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
+app.use(fileUpload());
 
 app.use('/api/parser', ourParser);
 
