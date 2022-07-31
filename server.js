b286e50de439036e5a89859a1d79a38a2b76b16b
Priyal2807 <priyalsobti@gmail.com>
2022-07-31 20:19:05 +0530

    deploying to heroku

--- server.js
+++ server.js
@@ -18,6 +18,9 @@ const PORT = process.env.port || 5000;
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, "client/build", "index.html"));
+    });
 }
 
 app.listen(PORT, () => {
