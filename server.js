946872e23b79955c02ee6c3ef0727fec5061ac82
Priyal2807 <priyalsobti@gmail.com>
2022-07-31 20:09:01 +0530

    changes for deploying to heroku

--- server.js
+++ server.js
@@ -11,10 +11,15 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cors());
 app.use(fileUpload());
-
 app.use('/api/parser', ourParser);
 
 const PORT = process.env.port || 5000;
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+}
+
+
 app.listen(PORT, () => {
     console.log(`app is listening at ${PORT}`);
 })
