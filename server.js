d5c72f85feb4c76993be7b77f1eb050312aa2c6c
Priyal2807 <priyalsobti@gmail.com>
2022-08-07 20:00:07 +0530

    deploying to heroku

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(fileUpload());
 app.use('/api/parser', ourParser);
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 
 
 if (process.env.NODE_ENV === 'production') {
